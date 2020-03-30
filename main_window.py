6c525e0a1491032f8a054ea8433133ef8c665ca6
Marina Carrizo <marinancarrizo@gmail.com>
2020-03-30 10:14:14 -0300

    Add ROI selection for traffic light in main_tracking.py

--- main_window.py
+++ main_window.py
@@ -1,8 +1,8 @@
 from tkinter import *
 from PIL import Image, ImageTk
 from tkinter import filedialog
-import object_detection as od
-import imageio
+#import object_detection as od
+#import imageio
 import cv2
 
 class Window(Frame):
