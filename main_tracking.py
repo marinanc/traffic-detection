8bb563a396d425ee5ca822dbd53982dea56f2060
Marina Carrizo <marinancarrizo@gmail.com>
2020-04-03 12:21:57 -0300

    Updated main_tracking.py to detect vehicle within ROI

--- main_tracking.py
+++ main_tracking.py
@@ -77,7 +77,7 @@ def find_center(x, y, w, h):
     return cx, cy
 
 
-cap = cv2.VideoCapture("cut3_sjuanycan.mp4")
+cap = cv2.VideoCapture("/home/mcarrizo/02.mp4")
 cv2.namedWindow("Video Original")
 cv2.setMouseCallback("Video Original", mouse_drawing)
 
@@ -86,57 +86,23 @@ substraction = cv2.createBackgroundSubtractorKNN()
 while True:
     ret, frame = cap.read()
     if point1 and point2:
-        line1 = cv2.line(frame, point1, point2, (255, 127, 0), 3)
+        rectangle1 = cv2.rectangle(frame, point1, point2, (255, 127, 0), 2)
     tempo = float(1 / delay)
     sleep(tempo)
 
-    # Traffic light ROI
-    image_np = cv2.bilateralFilter(frame, 5, 50, 100)  # smoothing filter
-    img = image_np[285:int(cap_region_y_end * image_np.shape[0]) - 100,  # x
-          int(cap_region_x_begin * image_np.shape[1]) - 180:image_np.shape[1] - 520]  # clip the ROI
+    if point1 and point2:
+        img = frame[point1[1]:point2[1], point1[0]:point2[0]]
 
-    cv2.imshow('mask', img)
+        grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(grey, (3, 3), 5)
+        img_sub = substraction.apply(blur)
+        dilat = cv2.dilate(img_sub, np.ones((3, 3)))  # (5,5)
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+        dilated = cv2.morphologyEx(dilat, cv2.MORPH_CLOSE, kernel)
+        dilated = cv2.morphologyEx(dilated, cv2.MORPH_CLOSE, kernel)
 
-    # Convert BGR to HSV
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        contour, h = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Range of red color in HSV
-    lower_red = (170, 100, 100)
-    upper_red = (179, 255, 255)
-    # Treshold the HSV image to get only red color
-    mask = cv2.inRange(hsv, lower_red, upper_red)
-    # Bitwise-AND mask and original image
-    # res = cv2.bitwise_and(frame,frame, mask= mask)
-
-    # Object area
-    moments = cv2.moments(mask)
-    area = moments['m00']
-
-    if area > 170000:
-        # Object center (x,y)
-        x = int(moments['m10'] / moments['m00'])
-        y = int(moments['m01'] / moments['m00'])
-
-        # Dibujamos una marca en el centro del objeto
-        cv2.rectangle(img, (x, y), (x + 2, y + 2), (0, 0, 255), 2)
-
-    # Showing the mask
-    cv2.imshow('mask2', mask)
-
-    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(grey, (3, 3), 5)
-    img_sub = substraction.apply(blur)
-    # cv2.imshow('substraction', img_sub)
-    dilat = cv2.dilate(img_sub, np.ones((3, 3)))  # (5,5)
-    # cv2.imshow('dilate', dilat)
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-    dilated = cv2.morphologyEx(dilat, cv2.MORPH_CLOSE, kernel)
-    dilated = cv2.morphologyEx(dilated, cv2.MORPH_CLOSE, kernel)
-
-    contour, h = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    # Red light
-    if cv2.countNonZero(mask) > 0:
         for (i, c) in enumerate(contour):
             (x, y, w, h) = cv2.boundingRect(c)
             validate_contour = (w >= w_min) and (h >= h_min)
@@ -146,33 +112,27 @@ while True:
             center = find_center(x, y, w, h)
             detec.append(center)
             cv2.circle(frame, center, 4, (0, 0, 255), -1)
+            alpha = 0.4
+
             for (x, y) in detec:
                 if point1 and point2:
                     if (point2[1]+offset) > y > (point1[1]-offset) and point2[0] > x > point1[0]:
                         vehicle += 1
-                        success, img_cap = cap.read()
+                        overlay = frame.copy()
+                        cv2.rectangle(overlay, point1, point2, (0, 0, 255), -1)  # A filled rectangle
+
+                        alpha = 0.4  # Transparency factor.
+
+                        # Following line overlays transparent rectangle over the image
+                        image_new = cv2.addWeighted(overlay, alpha, frame, 1 - alpha, 0, frame)
+                        detec.remove((x,y))
+                        print("Vehiculo mal estacionado")
                         # Snapshot
-                        if success:
-                            cv2.imwrite(os.path.join(os.getcwd(), '%d.png') % vehicle, img_cap)
-                            # if (y<(pos_line1+offset) and y>(pos_line1-offset) and x<1100 and x>700)
-                            # or (y<(pos_line2+offset) and y>(pos_line2-offset) and x<900 and x>0)
-                            # or (y<(pos_line3+offset) and y>(pos_line3-offset) and x<1850 and x>1100):
-                            # #Video
-                            # out = cv2.VideoWriter(os.path.join(os.getcwd(), '%d.avi') % vehicle,cv2.VideoWriter_fourcc('M','J','P','G'), 5.0, (int(cap.get(3)), int(cap.get(4))))
-                            # start_time = time.time()
-                            # while( int(time.time() - start_time) < capture_duration ):
-                            #   ret, frame = cap.read()
-                            #   if ret==True:
-                            #       out.write(frame)
-                            #   else:
-                            #       break
-                        if (point2[1]+offset) > y > (point1[1]-offset) and point2[0] > x > point1[0]:
-                            cv2.line(frame, point1, point2, (0, 0, 255), 3)
 
     cv2.putText(frame, "VEHICULOS: " + str(vehicle), (450, 70), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 5)
     cv2.imshow("Video Original", frame)
     # cv2.imshow("Video Original" , cv2.resize(frame, (1000, 600)))
-    cv2.imshow("Detectar", cv2.resize(dilated, (1000, 600)))
+    #cv2.imshow("Detectar", cv2.resize(dilated, (1000, 600)))
 
     if cv2.waitKey(1) == 27:
         break
